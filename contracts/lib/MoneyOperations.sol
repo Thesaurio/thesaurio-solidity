7888ec62d605770cda25e65a95c6f72ca8beab47
Jérémy Seban <jseban@francechain.fr>
2018-04-23 18:30:40 +0200

    Updated 0.0.7 - Changed uint to uint256 everywhere

--- contracts/lib/MoneyOperations.sol
+++ contracts/lib/MoneyOperations.sol
@@ -4,8 +4,8 @@ library MoneyOperations {
   /**
   * @dev Divides two numbers with 18 decimals, represented as uints (e.g. ether or token values)
   */
-  uint constant ETHER_PRECISION = 10 ** 18;
-  function ethdiv(uint x, uint y) internal pure returns (uint z) {
+  uint256 constant ETHER_PRECISION = 10 ** 18;
+  function ethdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
     // Put x to the 36th order of magnitude, so natural division will put it back to the 18th
     // Adding y/2 before putting x back to the 18th order of magnitude is necessary to force the EVM to round up instead of down
     z = _add(_mul(x, ETHER_PRECISION), y / 2) / y;
@@ -14,8 +14,8 @@ library MoneyOperations {
   /**
   * @dev Divides two numbers with 2 decimals, represented as uints (e.g. fiat)
   */
-  uint constant FIAT_PRECISION = 10 ** 2;
-  function fiatdiv(uint x, uint y) internal pure returns (uint z) {
+  uint256 constant FIAT_PRECISION = 10 ** 2;
+  function fiatdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
     // Put x to the 4th order of magnitude, so natural division will put it back to the 2th
     // Adding y/2 before putting x back to the 18th order of magnitude is necessary to force the EVM to round up instead of down
     z = _add(_mul(x, FIAT_PRECISION), y / 2) / y;
--- contracts/lib/PricesInterface.sol
+++ contracts/lib/PricesInterface.sol
@@ -2,19 +2,19 @@ pragma solidity ^0.4.21;
 
 contract PricesInterface {
   event PricesUpdated(
-    uint updateTimestamp,
-    uint eurPrice,
-    uint usdPrice,
-    uint cnyPrice,
-    uint jpyPrice,
-    uint gbpPrice,
-    uint rubPrice
+    uint256 updateTimestamp,
+    uint256 eurPrice,
+    uint256 usdPrice,
+    uint256 cnyPrice,
+    uint256 jpyPrice,
+    uint256 gbpPrice,
+    uint256 rubPrice
   );
 
-  function eurPrice() public view returns (uint);
-  function usdPrice() public view returns (uint);
-  function cnyPrice() public view returns (uint);
-  function jpyPrice() public view returns (uint);
-  function gbpPrice() public view returns (uint);
-  function rubPrice() public view returns (uint);
+  function eurPrice() public view returns (uint256);
+  function usdPrice() public view returns (uint256);
+  function cnyPrice() public view returns (uint256);
+  function jpyPrice() public view returns (uint256);
+  function gbpPrice() public view returns (uint256);
+  function rubPrice() public view returns (uint256);
 }
